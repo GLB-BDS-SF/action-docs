67273b66b5cf18553b4b046fb03457a5bce8f43e
Laurent Gil <lg7983@gmail.com>
2023-01-30 10:27:19 +0000

    fix: display github_ref and git fetch
    Signed-off-by: Laurent Gil <lg7983@gmail.com>

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -85,14 +85,8 @@ if [ "${INPUT_ACTION_DOCS_GIT_PUSH}" = "true" ]; then
   git_commit
   # git switch -c "${GITHUB_HEAD_REF}"
   git checkout -b "${GITHUB_HEAD_REF}"
-  debug_message "ICICI ${GITHUB_HEAD_REF}"
-  git fetch -v
   git fetch --unshallow
-  git fetch -v
-
-  debug_message "LAAAA"
   git push origin HEAD:"${GITHUB_HEAD_REF}" -f
-  debug_message "FIN"
 
 else
   git_changed
